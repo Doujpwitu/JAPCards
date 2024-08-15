363a6c979f631b96ef241fd8cd9c9cdf19105dc5
Ma3ri4 <94155834+Doujpwitu@users.noreply.github.com>
2024-08-15 13:14:04 +0300

    input css

--- script.js
+++ script.js
@@ -4,8 +4,16 @@ document.getElementById("check").onclick = function(){
     userinput = document.getElementById("matching").value;
     if(userinput==='おはよう'){
         console.log('Correct answer!')
+        document.getElementById('message').innerHTML = 'إجابة صحيحة!'
+        document.getElementById('matching').classList.remove('wrong')
+        document.getElementById('matching').classList.add('correct')
+        document.getElementById('matching').setAttribute( "disabled", "true" );
+        
     }else{
         console.log('Try again')
+        document.getElementById('message').innerHTML = '..حاول مرة أخرى'
+        document.getElementById('matching').classList.add('wrong')
     }
+    
 }
 
