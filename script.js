6f8b377f41e82581f3ab9949cb0c1a4149422690
Ma3ri4 <94155834+Doujpwitu@users.noreply.github.com>
2024-08-15 14:41:01 +0300

    added third card

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
 const checkButtons = document.getElementsByClassName("check")
 const entryFields = document.getElementsByClassName("entry")
 const messageOnEntry = document.getElementsByClassName("message")
-console.log(entryFields)
 const entryQandA = {
     entryCard_1:
         {
         Question:"おはよう",
-        id:0,
         userAnswer:entryFields[0],
         messageBox:messageOnEntry[0]
         }
@@ -14,36 +12,34 @@ const entryQandA = {
     entryCard_2:
     {
         Question:"にほん",
-        id:1,
         userAnswer:entryFields[1],
         messageBox:messageOnEntry[1]
+    },
+    entryCard_3:
+    {
+        Question:"日本",
+        userAnswer:entryFields[2],
+        messageBox:messageOnEntry[2]
     }
     
 };
 
-console.log(entryQandA)
 function onCheckClick(){
     const parentID = $(this).parent().attr('id');
     const domParent = document.getElementById(parentID)
-    console.log(parentID)
     let userinput = entryQandA[parentID].userAnswer.value;
-    console.log(userinput)
-    console.log(entryQandA[parentID].Question)
     if(userinput.trim()===entryQandA[parentID].Question){
-        console.log('Correct answer!')
         entryQandA[parentID].messageBox.innerHTML = 'إجابة صحيحة!'
         entryQandA[parentID].userAnswer.classList.remove('wrong')
         entryQandA[parentID].userAnswer.classList.add('correct')
         entryQandA[parentID].userAnswer.setAttribute( "disabled", "true" );
         
     }else{
-        console.log('Try again')
         entryQandA[parentID].messageBox.innerHTML = '..حاول مرة أخرى'
         entryQandA[parentID].userAnswer.classList.add('wrong')
     }
     
 }
-
 for(let i = 0; i < checkButtons.length; i++){
     checkButtons[i]
     .addEventListener("click",onCheckClick);
